a2ef8159655ebfffc45c0413ef792c79ed563e2a
redx099 <ferz099@mail.ru>
2024-03-31 12:32:49 +0500

    Убирает лишний checkAuthAction

--- src/pages/login/model.ts
+++ src/pages/login/model.ts
@@ -2,7 +2,6 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import {PromiseProps, UserProps} from '../../shared/types';
 import {redirectToRoute, token} from '../../shared/utils';
 import {Path} from '../../shared/config';
-import {checkAuthAction} from '../../shared/api';
 
 type AuthData = {
   email: string;
@@ -18,7 +17,6 @@ export const loginAction = createAsyncThunk<UserProps, AuthData, PromiseProps> (
     });
     token.save(data.token);
     dispatch(redirectToRoute(Path.Main));
-    dispatch(checkAuthAction());
     return data;
   }
 );
